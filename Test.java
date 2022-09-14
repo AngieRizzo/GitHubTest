39ac8562d817ac1fc9a74225eb5b6edf24d2241b
gtcha2 <aron7628@gmail.com>
2022-09-14 11:23:59 -0500

    system output change in void function

--- Test.java
+++ Test.java
@@ -4,5 +4,6 @@ public class Test {
   public static void main(String[] args) {
     System.out.println("Is this working?");
     System.out.println("Yes");
+    System.out.println("test1_aaron")
   }
 }
